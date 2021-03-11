732c8793d6be4a4d3f681a94bf38d1ef4322dcfc
Sean Blankenburgh <6010924@mborijnland.nl>
2021-03-11 13:29:07 +0100

    Revert "files van de navigatiebar toegevoegd"
    This reverts commit 4d7e11153bb8a66a51c6772dffaef20ca2cfe050.

--- Basispagina/Triple-Energy-main/navagatie bar/js/global.js
+++
@@ -1,8 +0,0 @@
-// Hamburger Menu
-function openBtn(){
-    document.getElementById("menu").style.width = "100vw"
-}
-
-function closeBtn(){
-    document.getElementById("menu").style.width = "0"
-}
